0558bb86504d1fd5f480040e57719e1761783835
Hennico <nzanardo@fi.uba.ar>
2024-06-15 00:13:53 -0300

    Agregar inicio de persistencia de filtro

--- data/profile.py
+++ data/profile.py
@@ -16,3 +16,12 @@ class Profile(BaseModel):
    longitud: float
    like_counter: int
    superlike_counter: int
+
+class Filter(BaseException):
+   userid: str
+   gender: str
+   age_from: int
+   age_to: int
+   education: str
+   ethnicity: str
+   distance: float

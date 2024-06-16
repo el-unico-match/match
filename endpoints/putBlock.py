3bcd39a1e5185afba06dd368a1e3f6aa4333a6fb
Martin Hernan Gomez <mhgomez@fi.uba.ar>
2024-06-16 09:53:19 -0300

    Removes commented code to create an empty swipe

--- endpoints/putBlock.py
+++ endpoints/putBlock.py
@@ -29,27 +29,6 @@ async def update_block_state(request: PutBlockRequest,client_db: any):
     if ( len(rows) == 0 ):
         raise HTTPException(status_code=404,detail="No se ha encontrado el match.") 		
 
-    # Here should I create the block for the other user?
-
-    # if ( len(rows) == 1 and rows[0]['userid_qualificator'] == request.swiper_userid ):
-
-    #     insert_empty_qualificated_user = '''
-    #         INSERT INTO matchs SET 
-    #             userid_qualificator = :swiped_userid, 
-    #             userid_qualificated = :swiper_userid, 
-    #             blocked = :isBlocked
-    #     '''
-    #     await client_db.execute(query = insert_empty_qualificated_user, values = values)
-
-
-    # if ( len(rows) == 1 and rows[0]['userid_qualificated'] == request.swiper_userid ):
-
-    #     insert_empty_qualificator_user = '''
-    #         INSERT INTO matchs
-    #         	SET userid_qualificated = :swiped_userid, userid_qualificator = :swiper_userid, blocked = :isBlocked
-    #     '''
-    #     await client_db.execute(query = insert_empty_qualificator_user, values = values)
-
     update_block_status = '''
         UPDATE matchs 
         SET blocked = :isBlocked

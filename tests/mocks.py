13fa4588ccea12fd163b8ad388665ea4914c775c
diegofiuba <diegotournet@gmail.com>
2024-06-27 13:22:52 -0300

    tests

--- tests/mocks.py
+++ tests/mocks.py
@@ -35,3 +35,45 @@ class Mock:
   "like_counter": 0,
   "superlike_counter": 0
         }
+		
+    async def fetch_all(self,query,values):
+        #sql_query = '''
+        #Select orig.userid_qualificator userid_1, orig.userid_qualificated userid_2,
+        #       orig.qualification qualification_1, dest.qualification qualification_2,
+        #       orig.qualification_date qualification_date_1, dest.qualification_date qualification_date_2,
+        #       pf1.username username_1, pf2.username username_2
+        #from matchs orig
+        #   inner join profiles pf1 on orig.userid_qualificator = pf1.userid
+        #   inner join matchs dest on orig.userid_qualificated = dest.userid_qualificator 
+        #                         and orig.userid_qualificator = dest.userid_qualificated
+        #   inner join profiles pf2 on orig.userid_qualificated = pf2.userid
+        #where orig.qualification = :like
+        #  and dest.qualification = :like
+        #  and orig.userid_qualificator = :id
+        #  and not orig.blocked and not dest.blocked
+        #order by orig.last_message_date desc
+        #'''
+        #print(query==sql_query)		
+        #if(query==sql_query):
+        #   print("..entra aca..")			
+           return self.execute_match_query(values)
+		   
+    def execute_match_query(self,values):
+        #print("valores:")
+        #print(values)
+        #print(type(values))	
+        if(values['id']=="4321"):
+           print("entra aca...")
+           matchs=[]
+           item={
+      "userid_1": "4321",
+      "username_1": "Angelina Jolie",
+      "qualification_1": "like",
+      "qualification_date_1": "2024-06-05T23:24:11.580459",
+      "userid_2": "3",
+      "username_2": "Ryan Gosling",
+      "qualification_2": "like",
+      "qualification_date_2": "2024-06-06T17:55:48.670889"
+      }         		   
+           matchs.append(item)
+           return matchs

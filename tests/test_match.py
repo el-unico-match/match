6831ecabe1c554867b3442418eb73fbf1e58432a
diegofiuba <diegotournet@gmail.com>
2024-07-04 11:28:16 -0300

    test

--- tests/test_match.py
+++ tests/test_match.py
@@ -90,23 +90,24 @@ def test_view_profile():
     assert data["like_counter"] == 4
     assert data["superlike_counter"] == 0
 
-#def test_create_existent_user_profile():
-#    response = client.post("/user/profile",
-#	json={
-#        "userid": "1234",	
-#        "username": "Luis",
-#        "gender": "Hombre",
-#        "looking_for": "Mujer",
-#        "age": 33,
-#        "education": "Universitaria",
-#        "ethnicity": "",
-#        "is_match_plus": True,
-#        "latitud": 23.3223,
-#        "longitud": 55.82,
-#        "like_counter": 0,
-#        "superlike_counter": 0
-#    })
-#    assert response.status_code == 404, response.text
+def test_block_profile():
+    response = client.put("/user/100/match/profile/block")
+
+    #print(response) 
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["userid"] == "100"
+    assert data["username"] == "Angelina Jolie"
+    assert data["gender"] == "Mujer"
+    assert data["looking_for"] == "Hombre"
+    assert data["age"] == 48
+    assert data["education"] == "Estudios universitarios"
+    assert data["ethnicity"] == ""	
+    assert data["is_match_plus"] == False
+    assert data["latitud"] == 5.3432
+    assert data["longitud"] == 7.846
+    assert data["like_counter"] == 4
+    assert data["superlike_counter"] == 0
 	
 def test_update_inexistent_user_profile():
     response = client.put("/user/profile/1234",

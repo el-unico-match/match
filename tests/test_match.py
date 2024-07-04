ebfe2ff5f9611b3a18d1e634649cddc8a2e0c38d
diegofiuba <diegotournet@gmail.com>
2024-07-04 11:44:42 -0300

    test

--- tests/test_match.py
+++ tests/test_match.py
@@ -127,6 +127,25 @@ def test_unblock_profile():
     assert data["longitud"] == 7.846
     assert data["like_counter"] == 4
     assert data["superlike_counter"] == 0
+
+def test_complete_profile():
+    response = client.put("/user/100/match/profile/complete")
+
+    #print(response) 
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["userid"] == "100"
+    assert data["username"] == "Angelina Jolie"
+    assert data["gender"] == "Mujer"
+    assert data["looking_for"] == "Hombre"
+    assert data["age"] == 48
+    assert data["education"] == "Estudios universitarios"
+    assert data["ethnicity"] == ""	
+    assert data["is_match_plus"] == False
+    assert data["latitud"] == 5.3432
+    assert data["longitud"] == 7.846
+    assert data["like_counter"] == 4
+    assert data["superlike_counter"] == 0
 	
 def test_update_inexistent_user_profile():
     response = client.put("/user/profile/1234",
